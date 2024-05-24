cf3d93fe98f37473be01caff185a6f4e8225e1ab
Memoryman2022 <rlacannon@gmail.com>
2024-05-24 13:20:30 +0200

    network access

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,10 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005/api";
+// src/config.js
+
+const API_URL =
+  window.location.hostname === "localhost"
+    ? import.meta.env.VITE_API_URL
+    : import.meta.env.VITE_API_IP_URL;
+
+console.log("API_URL:", API_URL);
 
 export { API_URL };
