970e1edab46972da5e51320d87bfc44c2b3070c1
Memoryman2022 <rlacannon@gmail.com>
2024-05-28 17:54:31 +0200

    Update environment configuration and WebSocket setup

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,6 +2,9 @@
 import { io } from "socket.io-client";
 import { API_URL } from "../config/index";
 
-const socket = io(API_URL.replace("/api", ""));
+const socket = io(API_URL.replace("/api", ""), {
+  transports: ["websocket", "polling"], // Ensure both transports are allowed
+  withCredentials: true, // Include credentials for CORS
+});
 
 export default socket;
