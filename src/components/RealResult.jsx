24fcecd92a358a6a458e6cbcf435052b8109feab
Memoryman2022 <rlacannon@gmail.com>
2024-06-02 12:05:57 +0200

    removed axios instance

--- src/components/RealResult.jsx
+++ src/components/RealResult.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import axiosInstance from "../utils/axiosInstance";
+import axios from "axios";
 import { AuthContext } from "../authContext/auth.context";
 import { API_URL } from "../config";
 import "../Css/RealResult.css";
@@ -13,7 +13,7 @@ function RealResult({ game, index }) {
 
   const handleSave = async () => {
     try {
-      await axiosInstance.post(`${API_URL}/realresults/${game.id}/result`, {
+      await axios.post(`${API_URL}/realresults/${game.id}/result`, {
         team1: game.team1,
         team2: game.team2,
         team1Score,
@@ -38,34 +38,26 @@ function RealResult({ game, index }) {
           type="number"
           value={team1Score}
           onChange={(e) => setTeam1Score(parseInt(e.target.value))}
-          disabled={!isAdmin}
         />
         <span>vs</span>
         <input
           type="number"
           value={team2Score}
           onChange={(e) => setTeam2Score(parseInt(e.target.value))}
-          disabled={!isAdmin}
         />
         <span>{game.team2}</span>
       </div>
       <div className="result-outcome">
-        {isAdmin ? (
-          <select value={outcome} onChange={(e) => setOutcome(e.target.value)}>
-            <option value="">Select Outcome</option>
-            <option value="team1 win">{game.team1} win</option>
-            <option value="draw">Draw</option>
-            <option value="team2 win">{game.team2} win</option>
-          </select>
-        ) : (
-          outcome || "No result yet"
-        )}
+        <select value={outcome} onChange={(e) => setOutcome(e.target.value)}>
+          <option value="">Select Outcome</option>
+          <option value="team1 win">{game.team1} win</option>
+          <option value="draw">Draw</option>
+          <option value="team2 win">{game.team2} win</option>
+        </select>
       </div>
-      {isAdmin && (
-        <button onClick={handleSave} className="save-button">
-          Save Result
-        </button>
-      )}
+      <button onClick={handleSave} className="save-button">
+        Save Result
+      </button>
     </div>
   );
 }
