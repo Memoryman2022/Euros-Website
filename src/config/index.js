02c4e47999bdc9a875b9f7b14429d03081b63e64
Memoryman2022 <rlacannon@gmail.com>
2024-06-05 20:09:23 +0200

    test deploy

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 // src/config/index.js
 
-const API_URL =
-  window.location.hostname === "localhost"
-    ? import.meta.env.VITE_API_URL_LOCAL
-    : import.meta.env.VITE_API_URL;
+const API_URL = "https://euros-backend.fly.dev";
+//   window.location.hostname === "localhost"
+//     ? import.meta.env.VITE_API_URL_LOCAL
+//     : import.meta.env.VITE_API_URL;
 
-console.log("API_URL:", API_URL);
+// console.log("API_URL:", API_URL);
 
 export { API_URL };
