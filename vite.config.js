5b43c8814bbd7657ed87fbe12327c517d7b2f3a0
Memoryman2022 <rlacannon@gmail.com>
2024-06-10 10:59:23 +0200

    pic uploads

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import dotenv from "dotenv";
+
+// dotenv.config(); // Load environment variables from .env file
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: "0.0.0.0",
+//     port: 5173,
+//     proxy: {
+//       "/api": {
+//         target: process.env.VITE_API_URL_LOCAL, // The backend server URL
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
@@ -11,7 +31,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL_LOCAL, // The backend server URL
+        target: process.env.VITE_API_URL, // Use the dynamically selected API URL
         changeOrigin: true,
         secure: false,
       },
