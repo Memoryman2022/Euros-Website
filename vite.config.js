b18271ed9569d18e9477c0c50e2ec5ffe526ca68
Memoryman2022 <rlacannon@gmail.com>
2024-06-16 14:38:24 +0200

    default scores

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import dotenv from "dotenv";
-
-// dotenv.config(); // Load environment variables from .env file
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: "0.0.0.0",
-//     port: 5173,
-//     proxy: {
-//       "/api": {
-//         target: process.env.VITE_API_URL_LOCAL, // The backend server URL
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
@@ -31,10 +11,30 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL, // Use the dynamically selected API URL
+        target: process.env.VITE_API_URL_LOCAL, // The backend server URL
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import dotenv from "dotenv";
+
+// dotenv.config(); // Load environment variables from .env file
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: "0.0.0.0",
+//     port: 5173,
+//     proxy: {
+//       "/api": {
+//         target: process.env.VITE_API_URL, // Use the dynamically selected API URL
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
